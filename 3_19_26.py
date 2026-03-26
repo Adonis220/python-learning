47383a77a56af5e8c55c762fb3f16310efa377c6
Adonis Robinson <ALCorrothers01@indianatech.net>
2026-03-26 12:33:32 -0400

    homework

---
+++ 3_19_26.py
@@ -0,0 +1,35 @@
+
+for i in range(10):
+   print(i)
+   if i == 5:
+      break
+
+
+# Find and print the first even number in this list.
+# Stop searching once you find it.
+numbers = [3, 7, 9, 2, 11, 4, 13]
+# Your loop with break here
+
+
+
+
+correct_password = "secure_123"
+logged = False
+password = input("please enter password")
+   if password == secure_password:
+      print(" ")
+      logged = True
+
+
+# Process only words that meet ALL of these criteria:
+# - Not empty
+# - Longer than 2 characters
+# - Does NOT start with 'x'
+words = ["hi", "xerox", "", "python", "xy", "code", "x", "algorithm"]
+# Print each valid word. Use continue for invalid ones.
+for word in words 
+   if word == "" or len word <=2 or word.startswith("x"):
+    continue
+print(f"valid words" word)
+
+
